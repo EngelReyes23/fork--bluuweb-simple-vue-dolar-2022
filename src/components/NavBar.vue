42d56a3f7b4fc8950c3de0cc0a9621c8708698de
bluuweb <contacto@bluuweb.cl>
2022-09-10 17:08:39 -0400

    fin frontend api

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,34 +1,26 @@
 <script setup>
-import IconDarkMode from '@/components/icons/IconDarkMode.vue';
-import IconLightMode from '@/components/icons/IconLightMode.vue';
-import { ref } from 'vue';
+import IconDarkMode from "@/components/icons/IconDarkMode.vue";
+import IconLightMode from "@/components/icons/IconLightMode.vue";
+import { useDarkMode } from "@/composables/useDarkMode.js";
 
-// INFO: Nunca he usado Vue xD, soy mas de React
-
-// Verifica si el tema del SO es DarkMode
-const isDarkMode = ref(window.matchMedia('(prefers-color-scheme: dark)').matches)
-
-const toggleDarkMode = () => {
-  isDarkMode.value = !isDarkMode.value
-  isDarkMode.value ? document.body.classList.add('dark') : document.body.classList.remove('dark')
-}
+const { changeDarkMode, toggleDarkMode } = useDarkMode();
 </script>
 
 <template>
-  <header class="bg-gray-800 text-white p-4 mb-5">
-    <div class="max-w-7xl flex justify-between items-center mx-auto">
-      <h1
-        class="text-2xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500"
-      >
-        Logo API Dollar
-      </h1>
-      <button
-        @click="toggleDarkMode"
-        class="active:ring-rose-400 hover:ring rounded-lg p-0.5 outline-none ring-indigo-500 dark:text-white"
-      >
-        <IconDarkMode v-if="isDarkMode" />
-        <IconLightMode v-else />
-      </button>
-    </div>
-  </header>
+    <header class="p-4 mb-5 text-white bg-gray-800">
+        <div class="flex items-center justify-between mx-auto max-w-7xl">
+            <h1
+                class="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500"
+            >
+                API Dollar
+            </h1>
+            <button
+                @click="changeDarkMode"
+                class="active:ring-rose-400 hover:ring rounded-lg p-0.5 outline-none ring-indigo-500 dark:text-white"
+            >
+                <IconDarkMode v-if="toggleDarkMode" />
+                <IconLightMode v-else />
+            </button>
+        </div>
+    </header>
 </template>
